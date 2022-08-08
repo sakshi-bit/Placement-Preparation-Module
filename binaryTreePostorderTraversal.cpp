4cd58744d7b34d66b75dc9025e1ca7a975da66cf
Sakshi Srivastava <69712091+sakshi-bit@users.noreply.github.com>
2022-08-08 17:17:14 +0530

    Create binaryTreePostorderTraversal.cpp

---
+++ binaryTreePostorderTraversal.cpp
@@ -0,0 +1,15 @@
+void postorder(TreeNode* root,vector<int>&ans){
+    if(root==NULL){
+        return;
+    }
+    postorder(root->left,ans);
+    postorder(root->right,ans);
+    ans.push_back(root->val);
+
+}
+vector<int> solve(TreeNode* root){
+//CODE HERE
+vector<int>ans;
+postorder(root,ans);
+return ans; 
+}
